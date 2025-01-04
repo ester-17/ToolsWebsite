109204dbd96dc18bdc309d69b4ccee3a443bebf5
ester-17 <139085601+ester-17@users.noreply.github.com>
2025-01-04 17:12:02 -0300

    register creation

--- src/Models/User.php
+++ src/Models/User.php
@@ -4,11 +4,11 @@ require_once "src/Models/Model.php";
 
 class User extends Model {
 
-    protected $tableName = 'users';
+    protected $tableName = 'operators';
 
     public function insert($nome, $email, $matricula, $password){
 
-        $result = $this->connection->query("INSERT INTO $this->tableName (name, email, phone, password) VALUES ('$nome', '$email', '$matricula', '$password')");
+        $result = $this->connection->query("INSERT INTO $this->tableName (nome, email, matricula, password) VALUES ('$nome', '$email', '$matricula', '$password')");
 
         return $result;
     }
